03798013bbe15e9049014072d370c5db94797549
Shazadkhan1999 <Shazadkhan40005@gmail.com>
2025-10-07 17:42:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+
+// requred function
+  fubction getmessage() {
+	return new Promise((resolve) => {
+	  setTimeout(() => {
+		  resolve("hello world");
+	  }, 1000);
+  });
+         // use promise
+	  getMessage().then((message) => {
+		  document.getElementById("output").textContent = message;
+		  
+	  });
+	  
